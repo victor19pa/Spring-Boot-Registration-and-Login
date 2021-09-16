bc57f56f77badb13992e6779f02b6d9bcd6a69bb
victor19pa <vpinedaa@unah.hn>
2021-09-15 19:32:52 -0600

    userRepositoryTests

---
+++ CodeJavaApp/src/test/java/com/example/demo/UserRepositoryTests.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+ 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+	@Autowired
+    private TestEntityManager entityManager;
+     
+    @Autowired
+    private UserRepository repo;
+    
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setEmail("victor2021@gmail.com");
+        user.setPassword("victor2021");
+        user.setFirstName("Victor");
+        user.setLastName("Pineda");
+         
+        User savedUser = repo.save(user);
+         
+        User existUser = entityManager.find(User.class, savedUser.getId());
+         
+        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
+         
+    }
+}
